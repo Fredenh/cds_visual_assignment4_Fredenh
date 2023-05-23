9f89bf45b722d3065ed33bf84e946cf99e2a34e3
Fredenh <frede.normann@gmail.com>
2023-05-23 22:06:11 +0200

    updates to branch

---
+++ src/nnclass.py
@@ -0,0 +1,75 @@
+# Importing necessary tools and packages
+import os
+import numpy as np
+import cv2
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+from sklearn.linear_model import LogisticRegression
+from sklearn.neural_network import MLPClassifier
+
+def main():
+    # Defining the folder names corresponding to each class
+    folder_names = ["badminton", "padel", "squash", "table_tennis", "tennis"]
+
+    # Initializing the data and labels by creating empty lists with each
+    X_dataset = []
+    y_labels = []
+
+    # Loading and preprocessing the images
+    for label, folder in enumerate(folder_names):
+        # Full path to folder with labels
+        folder_path = os.path.join(".", "in", folder)
+        # Iterating through every file 
+        for filename in os.listdir(folder_path):
+            # Path to image files
+            image_path = os.path.join(folder_path, filename)
+            # Loading image with OpenCV's cv2.IMREAD_GRAYSCALE
+            image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+            # Resizing images to a desired size. More to the point of consistent image sizes since they all are of different origin on Pexels
+            resized_image = cv2.resize(image, (255, 255))
+            # Appending the preprocessed images and identified labels to the empty lists from before
+            X_dataset.append(resized_image)
+            y_labels.append(label)
+
+    # Converting the data to numpy arrays
+    X_dataset = np.array(X_dataset)
+    y_labels = np.array(y_labels)
+
+    # Reshaping the input data
+    nsamples, nx, ny = X_dataset.shape
+    X_dataset = X_dataset.reshape((nsamples, nx * ny))
+
+    # Scaling the input data
+    X_scaled = X_dataset / 255.0
+
+    # Splitting the data into training and testing sets
+    X_train, X_test, y_train, y_test = train_test_split(X_scaled, y_labels, test_size=0.2, random_state=42)
+
+    # Building the neural network classifier 
+    clf = MLPClassifier(random_state=42, # Setting random seed
+                        hidden_layer_sizes=(100, 10), # Defining size of hidden layers
+                        learning_rate="adaptive", # Adaptive = it will adjust during training
+                        early_stopping=False, # Not stopping the training since there isnt much training data
+                        verbose=True, 
+                        max_iter=20).fit(X_train, y_train) # Sets the maximum iterations to 20 if it isnt stopped earlier
+
+    # Making predictions based on the testing set
+    y_pred = clf.predict(X_test)
+
+    # Generating a classification report
+    report = classification_report(y_test,
+                                   y_pred,
+                                   target_names=folder_names) # Using the previously defined labels
+
+    # Saving the classification report
+    folder_path = os.path.join(".", "out")
+    file_name = "neural_network_classifier.txt"
+    file_path = os.path.join(folder_path, file_name)
+
+    # "Writing" the classification report
+    with open(file_path, "w") as f:
+        f.write(report)
+    print("Report saved") 
+
+if __name__ == "__main__":
+    main()
---
+++ src/scrape.py
@@ -0,0 +1,89 @@
+import argparse
+import json
+import os
+import requests
+
+def parse_arguments():
+    # Defining command line argument parser
+    parser = argparse.ArgumentParser()
+    # Adding --query argument with default value and help message. This is for the sake of scraping every of the 5 sports at ones own will
+    parser.add_argument('--query', type=str, default='padel tennis',
+                        help='Search query for Pexels')
+    # Parsing the command line arguments and returning the parsed query
+    args = parser.parse_args()
+    return args.query
+
+def main():
+    # Parsing the query from command line arguments
+    query = parse_arguments()
+
+    # Setting up the search parameters for the images
+    per_page = 80  # A set maximum value for per_page is 80
+    total_images = 240 # Total amount of images scraped before stopping
+    # My personal API key 
+    api_key = "Wp6anIfOgkxac7KFJN8AbUvRGRBA7czoWx6yNhByNuRykla6XPTvPkZc"  
+
+    # This dictionary shows which preset queries to use to get the exact same data as me. 
+    query_to_folder = {
+        "badminton play": "badminton",
+        "padel tennis": "padel",
+        "squash sport": "squash",
+        "table tennis": "table_tennis",
+        "tennis sport": "tennis"
+    }
+
+    # Getting the output folder based on the search query
+    out_folder = query_to_folder.get(query, "other") # the "other" refers to if an argument is added in the command line that does not correspond to the preset queries above. The line simply makes a new folder called "out", and places the scraped images there.
+
+    # Specifying the output folder path
+    outpath = os.path.join(".", "in", out_folder)
+
+    # Calculating the number of pages needed to reach 240 images
+    num_pages = (total_images + per_page - 1) // per_page
+
+    # Initializing the image count
+    image_count = 0
+
+    # For loop making API requests for each page
+    for page in range(1, num_pages + 1):
+        # Constructing the API request URL with the query, per_page, and page which were defined before
+        url = f"https://api.pexels.com/v1/search?query={query}&per_page={per_page}&page={page}"
+        # Setting the headers with the API key
+        headers = {"Authorization": api_key}
+        # Sending the GET request to the Pexels API
+        response = requests.get(url, headers=headers)
+        # Parsing the JSON response
+        data = json.loads(response.text)
+
+        # Processing the API response and extracting image URLs
+        # Making empty list for later appending
+        image_urls = []
+        for photo in data['photos']:
+            image_urls.append(photo['src']['original'])
+
+        # Downloading images
+        for i, image_url in enumerate(image_urls):
+            # This if statement checks if the desired number of images has been reached. If it has, then it stops
+            if image_count >= total_images:
+                break
+
+            # Sending a GET request to download the image
+            response = requests.get(image_url, stream=True)
+            if response.status_code == 200:
+                # Making complete image path with f string to create a nicely indexed list of images in the folder
+                image_path = os.path.join(outpath, f"image_{image_count + 1}.jpg")
+                # Saving the image file by "writing" it as a txt
+                with open(image_path, "wb") as file:
+                    for chunk in response.iter_content(1024):
+                        file.write(chunk)
+                # Printing success message for each successfully downloaded image
+                print(f"Image {image_count + 1} downloaded successfully.")
+            else:
+                # Printing failure message 
+                print(f"Failed to download Image {image_count + 1}.")
+
+            # Keeping track of the image count
+            image_count += 1
+
+if __name__ == '__main__':
+    main()
